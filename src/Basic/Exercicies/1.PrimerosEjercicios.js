a2825de6a4ed936f7b10db2bc082425c6370f88e
Jaguar Dev <dev@jaguar-web.local>
2026-06-10 19:40:49 -0500

    Main

---
+++ src/Basic/Exercicies/1.PrimerosEjercicios.js
@@ -0,0 +1,31 @@
+// Ejercicios básicos de JavaScript //
+
+// 1. Escribe 1 comentario en una linea y otro comentario en varias líneas
+
+// Este es un comentario de una sola línea  
+/*
+Este es un comentario de varias líneas
+que abarca más de una línea
+*/
+
+
+// 2. Declara una variable por cada tipo de dato 
+
+let numero = 42; // Número
+let texto = "Hola, mundo!"; 
+let booleano = true;
+let nulo = null; 
+let indefinido; 
+let simbolo = Symbol("miSimbolo"); 
+let objeto = { nombre: "Juan", edad: 30 };
+
+
+//3. imprime por consola el resultado y su tipode datode cada variable //
+console.log(numero, typeof numero);
+console.log(texto, typeof texto);
+console.log(booleano, typeof booleano);
+console.log(nulo, typeof nulo);
+console.log(indefinido, typeof indefinido);
+console.log(simbolo, typeof simbolo);
+console.log(objeto, typeof objeto);
+
