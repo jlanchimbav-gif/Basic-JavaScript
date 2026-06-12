34427f9fac90966b2cd1eb3a91f6f6518adaeddf
Jaguar Dev <dev@jaguar-web.local>
2026-06-11 23:16:19 -0500

    Main

---
+++ src/Basic/0.4 Operadores..js
@@ -0,0 +1,56 @@
+// Estos son los operadores de JavaScript //
+// los operadores son símbolos que realizan operaciones sobre uno o más operandos y devuelven un resultado.//
+
+// Operadores aritméticos //
+let a = 10;
+let b = 30;  
+console.log(a + b); // Suma: 40
+console.log(a - b); // Resta: -20
+console.log(a * b); // Multiplicación: 300
+console.log(a / b); // División: 0.333...
+console.log(a % b); // Módulo: 10
+
+// Operadores de incremento y decremento
+let c = 5;
+console.log(c++); // Incremento: 5 (después de esta línea, c será 6)
+console.log(c--); // Decremento: 6 (después de esta línea, c volverá a ser 5)
+
+// Operadores de asignación //
+
+let E=27;
+E += 3; // E ahora es 30
+E -= 2; // E ahora es 28
+E *= 2; // E ahora es 56
+E /= 4; // E ahora es 14
+E %= 5; // E ahora es 4     
+
+// Operadores de comparación //
+
+console.log(a == b); // Igualdad: false
+console.log(a != b); // Desigualdad: true
+console.log(a > b); // Mayor que: false
+console.log(a < b); // Menor que: true
+console.log(a >= b); // Mayor o igual que: false
+console.log(a <= b); // Menor o igual que: true 
+console.log(a === b); // Igualdad estricta: false
+console.log(a !== b); // Desigualdad estricta: true
+
+// Operadores lógicos //
+// and: devuelve true si ambos operandos son verdaderos
+console.log(true && true); // true
+conainsole.log(true && false); // false
+console.log(false && true); // false
+
+// or: devuelve true si al menos uno de los operandos es verdadero
+console.log(true || true);  // true
+console.log(true || false); // true
+console.log(false || false); // false   
+
+// not: devuelve el valor opuesto del operando
+console.log(!true); // false
+console.log(!false); // true        
+
+
+
+
+
