32ca4b6a24b87ea71d83b53e019555ef2216d571
Jaguar Dev <dev@jaguar-web.local>
2026-06-21 18:14:02 -0500

    Main

--- src/Basic/0.8 sets.js
+++ src/Basic/0.8 sets.js
@@ -1,4 +1,6 @@
 // set //
+// es un tipo de estructura para guardar datos , no perite datos duplicados//
+
 let Myset= new setInterval
 
 // inicialazacion //
@@ -7,4 +9,29 @@ myset = new setInterval([Jorge,Alejandro,27,JaguarDev,true] )
 // Metodos comunes//
 // add y delete //
 
-Myset.add()
+Myset.add("jlanchimbav@umeni.edu.ec")
+console.log(myset)
+
+myset.delte()
+console.log(myset)
+
+// has
+
+console.log(myset.has(Alejandro))
+console.log(myset.has(JaguarDev))
+
+// size
+
+console.log(myset.size)
+
+//convertir un set en array 
+
+let myarray= array.from(myset)
+consolo.log(myarray)
+
+// covertir un array en set
+
+myset=new set(myarray)
+consolo.log(myset)
+
+
---
+++ src/Basic/0.9 Maps.js
@@ -0,0 +1,20 @@
+// Maps //
+// esta estructura se crea por el metedo (ki,value)dato y valor 
+// para cada dato una clave unica
+
+// declaracion 
+let mymap=new Map
+
+// inicializacion
+
+mymap=new Map([
+    ["nombre","Alejandro"],
+    ["nombre2","Jorge"],
+    ["edad","27"],
+    ["Alias","JaguarDev"],
+])
+
+console.log(mymap)
+
+// set agrega valores o actualiza valores 
+
