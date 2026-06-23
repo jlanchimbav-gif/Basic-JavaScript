456846d003a1a636f71936344ac598e5dd4a25b2
Jaguar Dev <dev@jaguar-web.local>
2026-06-23 16:56:33 -0500

    Main

--- src/Basic/10. loops.js
+++ src/Basic/10. loops.js
@@ -3,3 +3,15 @@
 //mientras la estructura cumpla algo verdadero o falso 
 // ayudan a automatizar procesos con grandes volumenes de datos.
 
+// for 
+
+for(let i=0; i<5; i++ ) {
+    console.log(`hola javascript ${i}`)
+}
+const numeros=[1,2,3,4,5]
+for(let i=0; i<5; i++ ) {
+    console.log(`hola javascript: ${numeros[i]}`)
+}
+
+// while 
+
